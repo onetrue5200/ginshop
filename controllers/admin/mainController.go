70725a9a2e4dc0b3252be90cd2e97797d450421e
onetrue <shane@192.168.0.101>
2022-08-22 01:26:13 +0800

    dynamically change the sidebar base on access

--- controllers/admin/mainController.go
+++ controllers/admin/mainController.go
@@ -1,15 +1,53 @@
 package admin
 
 import (
+	"encoding/json"
+	"ginshop/models"
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
 type MainController struct{}
 
 func (con MainController) Index(c *gin.Context) {
-	c.HTML(http.StatusOK, "admin/index.html", nil)
+	session := sessions.Default(c)
+	userinfo := session.Get("userinfo")
+	userinfoStr, ok := userinfo.(string)
+	if ok {
+		var userinfoStruct []models.Manager
+		json.Unmarshal([]byte(userinfoStr), &userinfoStruct)
+
+		roleId := userinfoStruct[0].RoleId
+
+		accessList := []models.Access{}
+		models.DB.Where("module_id=?", 0).Preload("AccessItem").Find(&accessList)
+		roleAccess := []models.RoleAccess{}
+		models.DB.Where("role_id=?", roleId).Find(&roleAccess)
+		roleAccessMap := make(map[int]int, len(roleAccess))
+		for _, v := range roleAccess {
+			roleAccessMap[v.AccessId] = roleId
+		}
+		for i := 0; i < len(accessList); i++ {
+			if _, ok := roleAccessMap[accessList[i].Id]; ok {
+				accessList[i].Checked = true
+			}
+			for j := 0; j < len(accessList[i].AccessItem); j++ {
+				if _, ok := roleAccessMap[accessList[i].AccessItem[j].Id]; ok {
+					accessList[i].AccessItem[j].Checked = true
+				}
+			}
+		}
+
+		c.HTML(http.StatusOK, "admin/index.html", gin.H{
+			"username":   userinfoStruct[0].Username,
+			"accessList": accessList,
+			"isSuper":    userinfoStruct[0].IsSuper,
+		})
+	} else {
+		c.Redirect(302, "/admin/login")
+	}
 }
 
 func (con MainController) Welcome(c *gin.Context) {
